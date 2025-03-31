3e6d6b44be87674cd211b6209cc2e1ada5475b71
dyna-blocks <dyna.blocks.beta.fan@gmail.com>
2025-04-01 10:20:37 +1030

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+// Add your Firebase project config here (from Firebase Console -> Project Settings)
+const firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
+  projectId: "YOUR_FIREBASE_PROJECT_ID",
+  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "YOUR_FIREBASE_APP_ID",
+  measurementId: "YOUR_FIREBASE_MEASUREMENT_ID"
+};
+
+// Initialize Firebase
+import { initializeApp } from "firebase/app";
+initializeApp(firebaseConfig);
