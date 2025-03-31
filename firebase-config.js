7b2244ba8343972bf4b56e3892f32772f4211ee7
dyna-blocks <dyna.blocks.beta.fan@gmail.com>
2025-04-01 10:22:12 +1030

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // firebase-config.js
 // Add your Firebase project config here (from Firebase Console -> Project Settings)
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID",
-  measurementId: "YOUR_FIREBASE_MEASUREMENT_ID"
+  apiKey: "AIzaSyA4yRkCYzUDc_GY_ou1UYJ7bIytLa-DwJY",
+  authDomain: "dynablocks-ce7d8.firebaseapp.com",
+  projectId: "dynablocks-ce7d8",
+  storageBucket: "dynablocks-ce7d8.firebasestorage.app",
+  messagingSenderId: "660768350715",
+  appId: "1:660768350715:web:7b0e02afcf6b7f4b0deb5e",
+  measurementId: "G-H3GCKHN9PF"
 };
 
 // Initialize Firebase
